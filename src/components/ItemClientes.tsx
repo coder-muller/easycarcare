d0bb0323b3f9ae3c39144efd62289b8351fbdaa7
caldeira04 <joao.p.caldeira@icloud.com>
2024-11-22 11:17:52 -0300

    Besides type errors, everything works

--- src/components/ItemClientes.tsx
+++ src/components/ItemClientes.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 interface Carro {
   marca: string;
   modelo: string;
@@ -12,32 +13,60 @@ interface Cliente {
   carro: Carro[];
 }
 
-const clientes: Cliente[] = []
-
-
 export default function ItemClientes() {
-  return (
-    <div>
-      <h1>Clientes</h1>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-        {clientes.map(cliente => (
-          <div key={cliente.nome} className="bg-blue-950 text-white my-6 lg:my-8 py-2 px-4">
-            <h2 className="uppercase text-xl text-center font-bold">{cliente.nome}</h2>
-            <div className="my-4">
-              <p>Telefone: {cliente.telefone}</p>
-              <p>Email: {cliente.email}</p>
-              <p>Endereço: {cliente.endereco}</p>
-              <p>Carros:</p>
-              <ul>
-                {cliente.carro.map(carro => (
-                  <li key={carro.placa}>{carro.marca} {carro.modelo} ({carro.placa}) - Cor: {carro.cor}</li>
-                ))}
-              </ul>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  )
 
+  const [clientes, setClientes]: Cliente[] = useState([])
+  const [loading, setLoading]: boolean = useState(true)
+
+  useEffect(() => {
+    const fetchClientes = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/cliente")
+        if (!response.ok) {
+          throw new Error("Erro ao buscar dados da agenda")
+        }
+        const data = await response.json()
+        setClientes(data)
+      } catch (error) {
+        console.error("Erro:", error)
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchClientes()
+  }, [])
+
+
+  return (
+    <>
+      {loading ? (
+        <p>Carregando...</p>
+      ) : (
+        <div>
+          <h1>Clientes</h1>
+          <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+            {clientes.map(cliente => (
+              <div key={cliente.nome} className="bg-blue-950 text-white my-6 lg:my-8 py-2 px-4">
+                <h2 className="uppercase text-xl text-center font-bold">{cliente.nome}</h2>
+                <div className="my-4">
+                  <p>Telefone: {cliente.telefone}</p>
+                  <p>Email: {cliente.email}</p>
+                  <p>Endereço: {cliente.endereco}</p>
+                  <p>Carros:</p>
+                  <ul>
+                    {cliente.carro.map(carro => (
+                      <li key={carro.placa}>{carro.marca} {carro.modelo} ({carro.placa}) - Cor: {carro.cor}</li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div >
+
+      )
+
+      }
+    </>
+  )
 }
--- src/components/ItemServices.tsx
+++ src/components/ItemServices.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react"
+
 interface Service {
   externa: boolean,
   code: string,
@@ -6,48 +8,76 @@ interface Service {
   produtos: string,
 }
 
-const services: Service[] = []
-
 export default function ItemServices() {
+
+  const [services, setServices]: Service[] = useState([])
+  const [loading, setLoading]: boolean = useState(true)
+
+  useEffect(() => {
+    const fetchServices = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/servico")
+        if (!response.ok) {
+          throw new Error("Erro ao buscar dados da agenda")
+        }
+        const data = await response.json()
+        setServices(data)
+      } catch (error) {
+        console.error("Erro:", error)
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchServices()
+  }, [])
+
 
   const listaServicosExterna = services.filter(servico => servico.externa)
   const listaServicosInterna = services.filter(servico => !servico.externa)
 
   return (
-    <div className="flex flex-col">
-      <button className="bg-blue-950 text-white py-2 px-4 rounded-lg m-auto mt-8">Adicionar serviço</button>
-      <div className="m-auto my-3 flex flex-row justify-between items-center">
-        <div className="flex flex-col w-2/5">
-          <h1 className="text-3xl font-bold text-center">Serviços Externos</h1>
-          {listaServicosExterna.map(servico => (
-            <section className="flex text-sm">
-              <div key={servico.nome} className="bg-blue-950 text-white my-6 lg:my-8 pl-4 flex flex-row border-r rounded-r-lg border-l rounded-l-lg">
-                <div className="py-4">
-                  <h2 className="uppercase text-lg text-center font-bold">{servico.nome}</h2>
-                  <p>{servico.desc}</p>
-                  <p>Produtos utilizados: {servico.produtos}</p>
-                </div>
-                <img src={`${servico.code}.png`} width="180px" height="125px" className="border-r rounded-r-lg" />
+    <>
+      {
+        loading ? (
+          <p> Carregando...</p >
+        ) :
+
+          <div className="flex flex-col">
+            <button className="bg-blue-950 text-white py-2 px-4 rounded-lg m-auto mt-8">Adicionar serviço</button>
+            <div className="m-auto my-3 flex flex-row justify-between items-center">
+              <div className="flex flex-col w-2/5">
+                <h1 className="text-3xl font-bold text-center">Serviços Externos</h1>
+                {listaServicosExterna.map(servico => (
+                  <section className="flex text-sm">
+                    <div key={servico.nome} className="bg-blue-950 text-white my-6 lg:my-8 pl-4 flex flex-row border-r rounded-r-lg border-l rounded-l-lg">
+                      <div className="py-4">
+                        <h2 className="uppercase text-lg text-center font-bold">{servico.nome}</h2>
+                        <p>{servico.desc}</p>
+                        <p>Produtos utilizados: {servico.produtos}</p>
+                      </div>
+                      <img src={`${servico.code}.png`} width="180px" height="125px" className="border-r rounded-r-lg" />
+                    </div>
+                  </section>
+                ))}
               </div>
-            </section>
-          ))}
-        </div>
-        <div className="flex flex-col w-2/5">
-          <h1 className="text-3xl font-bold text-center">Serviços Internos</h1>
-          {listaServicosInterna.map(servico => (
-            <section className="flex text-sm">
-              <div key={servico.nome} className="bg-blue-950 text-white my-6 lg:my-8 pl-4 flex flex-row border-r rounded-r-lg border-l rounded-l-lg">
-                <div className="py-4">
-                  <h2 className="uppercase text-lg text-center font-bold">{servico.nome}</h2>
-                  <p>{servico.desc}</p>
-                  <p>Produtos utilizados: {servico.produtos}</p>
-                </div>
-                <img src={`${servico.code}.png`} width="180px" height="125px" className="border-r rounded-r-lg" />
+              <div className="flex flex-col w-2/5">
+                <h1 className="text-3xl font-bold text-center">Serviços Internos</h1>
+                {listaServicosInterna.map(servico => (
+                  <section className="flex text-sm">
+                    <div key={servico.nome} className="bg-blue-950 text-white my-6 lg:my-8 pl-4 flex flex-row border-r rounded-r-lg border-l rounded-l-lg">
+                      <div className="py-4">
+                        <h2 className="uppercase text-lg text-center font-bold">{servico.nome}</h2>
+                        <p>{servico.desc}</p>
+                        <p>Produtos utilizados: {servico.produtos}</p>
+                      </div>
+                      <img src={`${servico.code}.png`} width="180px" height="125px" className="border-r rounded-r-lg" />
+                    </div>
+                  </section>
+                ))}
               </div>
-            </section>
-          ))}
-        </div>
-      </div>
-    </div>
+            </div>
+          </div>
+      }
+    </>
   )
 }
